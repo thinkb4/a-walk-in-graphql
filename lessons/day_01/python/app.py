6c32151abcb87e04417e14702e276d4c372e5383
Ezequiel Tejerina - ENDAVA <ezequiel.tejerina@wellsky.com>
2020-04-14 18:14:44 -0300

    Another Correction

--- lessons/day_01/python/app.py
+++ lessons/day_01/python/app.py
@@ -5,11 +5,6 @@ from schema import schema_query
 app = Flask(__name__)
 
 # Flask Rest & Graphql Routes
-# @app.route('/')
-# def hello_world():
-#     return 'Hello From Graphql Tutorial!'
-
-# /
 app.add_url_rule('/', view_func=GraphQLView.as_view(
     '',
     schema=schema_query, graphiql=True
