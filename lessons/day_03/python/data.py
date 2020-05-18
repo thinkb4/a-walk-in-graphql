d568d1b3c82b41b3033a75b0af6dd80d276e23d1
Ezequiel Tejerina - ENDAVA <ezequiel.tejerina@wellsky.com>
2020-05-18 14:22:29 -0300

    some requested change, removing an import

--- lessons/day_03/python/data.py
+++ lessons/day_03/python/data.py
@@ -1,6 +1,5 @@
 import os
 from flata import Flata
-from flata.storages import JSONStorage
 
 filename = os.path.abspath("../datasource/data.json")
-db = Flata(filename, storage=JSONStorage)
+db = Flata(filename)
