a2cc42e0481adb15d21947e7b801c88ca64774e9
fgriba <frg.14125*>
2020-06-10 14:07:14 -0300

    add Java DAY 5 legacy code

---
+++ lessons/day_06/java/src/main/java/com/example/DemoGraphQL/resolver/Mutation.java
@@ -0,0 +1,36 @@
+package com.example.DemoGraphQL.resolver;
+
+import com.example.DemoGraphQL.input.InputPersonCreate;
+import com.example.DemoGraphQL.input.InputSkillCreate;
+import com.example.DemoGraphQL.model.Person;
+import com.example.DemoGraphQL.model.Skill;
+import com.example.DemoGraphQL.service.PersonService;
+import com.example.DemoGraphQL.service.SkillService;
+import graphql.kickstart.tools.GraphQLMutationResolver;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * Top-Level resolver for Mutation
+ */
+@Component
+public class Mutation implements GraphQLMutationResolver {
+
+    private final SkillService skillService;
+    private final PersonService personService;
+
+    public Mutation(SkillService skillService, PersonService personService) {
+        this.skillService = skillService;
+        this.personService = personService;
+    }
+
+    public Skill createSkill(final InputSkillCreate input) {
+        return this.skillService.createSkill(Optional.ofNullable(input));
+    }
+
+    public Person createPerson(final InputPersonCreate input) {
+        return this.personService.createPerson(Optional.ofNullable(input));
+    }
+
+}
