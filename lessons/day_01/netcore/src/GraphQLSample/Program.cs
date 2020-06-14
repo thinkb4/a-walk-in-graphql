a88bc2d6bcd176d1de4b6913aca5e96a85082597
Cristian Buffa <cristianbuffa@hotmail.com>
2020-06-14 20:50:47 -0300

    Review's Fixes

--- lessons/day_01/netcore/src/GraphQLSample/Program.cs
+++ lessons/day_01/netcore/src/GraphQLSample/Program.cs
@@ -12,18 +12,6 @@ namespace GraphQLNetCore
 {
     public class Program
     {
-        //public static void Main(string[] args)
-        //{
-        //    CreateHostBuilder(args).Build().Run();
-        //}
-
-        //public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //    Host.CreateDefaultBuilder(args)
-        //        .ConfigureWebHostDefaults(webBuilder =>
-        //        {
-        //            webBuilder.UseStartup<Startup>();
-        //        });
-
         public static void Main(string[] args) => BuildWebHost(args).Run();
 
         public static IWebHost BuildWebHost(string[] args)
