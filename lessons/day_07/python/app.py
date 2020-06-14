ec633184e87d28b5f3823c6151afec2b48840ebb
Ezequiel Tejerina <ezequiel.tejerina@endava.com>
2020-06-13 21:55:36 -0300

    Solution from exercise of the previous day

--- lessons/day_07/python/app.py
+++ lessons/day_07/python/app.py
@@ -4,9 +4,13 @@ from resolvers import query, skill, eye_color, mutation, person, global_search
 
 
 # import schema from GraphQL file
-type_defs = load_schema_from_path("./schema.gql")
+type_def_global_search = load_schema_from_path("./schema/globalSearch.gql")
+type_def_person = load_schema_from_path("./schema/person.gql")
+type_def_skill = load_schema_from_path("./schema/skill.gql")
+type_def_schema = load_schema_from_path("./schema/schema.gql")
 
 schema = make_executable_schema(
-    type_defs, query, skill, eye_color, mutation, person, global_search
+    [type_def_global_search, type_def_person, type_def_skill, type_def_schema],
+    query, skill, eye_color, mutation, person, global_search
 )
 app = GraphQL(schema, debug=True)
