601c9e9272e7a721f8344c5651bb3fa5dfb7e297
Ezequiel Tejerina <tejerinae@gmail.com>
2020-06-22 12:21:17 -0300

    Refactor chenging sqlite with in memory db

--- lessons/day_01/python/models.py
+++ lessons/day_01/python/models.py
@@ -1,9 +1,21 @@
-from sqlalchemy import ForeignKey, Column, String
+from sqlalchemy import ForeignKey, Column, Integer, String, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
+
+person_friends = Table(
+    'person_friends', Base.metadata,
+    Column('person_id', String, ForeignKey('persons.id'), primary_key=True),
+    Column('friend_id', String, ForeignKey('persons.id'), primary_key=True)
+)
+
+person_skills = Table(
+    'person_skills', Base.metadata,
+    Column('person_id', String, ForeignKey('persons.id'), primary_key=True),
+    Column('skill_id', String, ForeignKey('skills.id'), primary_key=True)
+)
 
 
 class Skill(Base):
@@ -13,3 +25,24 @@ class Skill(Base):
     name = Column(String)
     parent = Column(String, ForeignKey('skills.id'))
     parent_skill = relationship("Skill", remote_side=[id], uselist=False)
+
+
+class Person(Base):
+    __tablename__ = 'persons'
+
+    id = Column(String, primary_key=True)
+    age = Column(Integer)
+    eyeColor = Column(String)
+    name = Column(String)
+    surname = Column(String)
+    email = Column(String)
+    friends = relationship(
+        "Person",
+        secondary=person_friends,
+        primaryjoin="Person.id==person_friends.c.person_id",
+        secondaryjoin="Person.id==person_friends.c.friend_id",
+        lazy='dynamic'
+    )
+    skills = relationship("Skill", secondary=person_skills, lazy='dynamic')
+    favSkill = Column(String, ForeignKey('skills.id'))
+    person_favSkill = relationship("Skill", uselist=False)
