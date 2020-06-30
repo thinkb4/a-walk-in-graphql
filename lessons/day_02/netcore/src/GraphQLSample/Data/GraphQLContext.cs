7dae8c74ba7af6384ea40b8d9142cb81c8b95e1a
Ignacio Aguilera <JoseIgnacio.Aguilera@endava.com>
2020-06-30 17:48:35 -0300

    move code to day2

---
+++ lessons/day_02/netcore/src/GraphQLSample/Data/GraphQLContext.cs
@@ -0,0 +1,57 @@
+﻿using GraphQLNetCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GraphQLNetCore.Data
+{
+   public class GraphQLContext : DbContext
+   {
+      public GraphQLContext(DbContextOptions<GraphQLContext> options) : base(options)
+      {
+      }
+
+      public DbSet<Skill> Skill { get; set; }
+      public DbSet<Person> Person { get; set; }
+
+      public void LoadFromJson()
+      {
+         var dirInfo = new System.IO.DirectoryInfo(@".\");
+         try
+         {
+            var response = (JObject)JsonConvert.DeserializeObject(File.ReadAllText((dirInfo.Parent).Parent.Parent.FullName + @"\datasource\data.json"));
+            var persons = JsonConvert.DeserializeObject<List<PersonData>>(response["persons"].ToString());
+            var skills = JsonConvert.DeserializeObject<List<SkillData>>(response["skills"].ToString());
+
+            Skill.AddRange(skills.Select(SkillData.ToEntity));
+            SaveChanges();
+
+            foreach (var person in persons)
+            {
+               var entity = PersonData.ToEntity(person);
+               entity.skills = Skill.Where(s => person.skills.Contains(s.id)).ToList();
+               entity.favSkill = Skill.Find(person.favSkill);
+               Person.Add(entity);
+            }
+
+            SaveChanges();
+
+            foreach (var person in persons)
+            {
+               var entity = Person.Find(person.id);
+               entity.friends = Person.Where(s => person.friends.Contains(s.id)).ToList();
+            }
+            
+            SaveChanges();
+
+         }
+         catch (System.Exception)
+         {
+         }
+      }
+   }
+}
+
---
+++ lessons/day_02/netcore/src/GraphQLSample/Repositories/PersonRepository.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GraphQLNetCore.Data;
+using GraphQLNetCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphQLNetCore.Repositories
+{
+   public class PersonRepository : IPersonRepository
+   {
+      private readonly IServiceScopeFactory _scopeFactory;
+      public PersonRepository(IServiceScopeFactory scopeFactory)
+      {
+         _scopeFactory = scopeFactory;
+      }
+
+      public Person Get(string id)
+      {
+         using (var scope = _scopeFactory.CreateScope())
+         using (var db = scope.ServiceProvider.GetRequiredService<GraphQLContext>())
+         {
+            return db.Person.FirstOrDefault(s => s.id == id);
+         }
+      }
+
+      public List<Person> GetAll()
+      {
+         using (var scope = _scopeFactory.CreateScope())
+         using (var db = scope.ServiceProvider.GetRequiredService<GraphQLContext>())
+         {
+            return db.Person.ToList();
+         }
+      }
+
+      public List<Person> GetFriends(string id)
+      {
+         using (var scope = _scopeFactory.CreateScope())
+         using (var db = scope.ServiceProvider.GetRequiredService<GraphQLContext>())
+         {
+            return db.Person.Include(x => x.friends).FirstOrDefault(s => s.id == id)?.friends;
+         }
+      }
+
+      public List<Skill> GetSkills(string id)
+      {
+         using (var scope = _scopeFactory.CreateScope())
+         using (var db = scope.ServiceProvider.GetRequiredService<GraphQLContext>())
+         {
+            return db.Person.Include(x => x.skills).FirstOrDefault(s => s.id == id)?.skills;
+         }
+      }
+
+      public Person GetRandom()
+      {
+         var rng = new System.Random();
+         using (var scope = _scopeFactory.CreateScope())
+         using (var db = scope.ServiceProvider.GetRequiredService<GraphQLContext>())
+         {
+            var indexRandom = rng.Next(db.Person.Count());
+            return db.Person.Skip(indexRandom).FirstOrDefault();
+         }
+      }
+   }
+}
