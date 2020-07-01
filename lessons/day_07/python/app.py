cf67701da8e6fc5343e3d22cc1815fa8e1f1e63d
Ezequiel Tejerina <tejerinae@gmail.com>
2020-07-01 09:26:41 -0300

    Update lessons/day_07/python/app.py
    Co-authored-by: Eloy Colella <eloy.colella@ordergroove.com>

--- lessons/day_07/python/app.py
+++ lessons/day_07/python/app.py
@@ -1,13 +1,13 @@
 from ariadne import make_executable_schema, load_schema_from_path
 from ariadne.asgi import GraphQL
 from resolvers import query, skill, eye_color, mutation, person, global_search
-
+working_dir = Path.cwd()
 
 # import schema from GraphQL file
-type_def_global_search = load_schema_from_path("./schema/globalSearch.gql")
-type_def_person = load_schema_from_path("./schema/person.gql")
-type_def_skill = load_schema_from_path("./schema/skill.gql")
-type_def_schema = load_schema_from_path("./schema/schema.gql")
+type_def_global_search = load_schema_from_path(working_dir/"schema/globalSearch.gql")
+type_def_person = load_schema_from_path(working_dir/"schema/person.gql")
+type_def_skill = load_schema_from_path(working_dir/"schema/skill.gql")
+type_def_schema = load_schema_from_path(working_dir/"schema/schema.gql")
 
 schema = make_executable_schema(
     [type_def_global_search, type_def_person, type_def_skill, type_def_schema],
