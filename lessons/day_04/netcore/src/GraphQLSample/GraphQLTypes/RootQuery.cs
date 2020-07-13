c498c99dd0de5e423426c9c33608a1514394c65f
Ignacio Aguilera <JoseIgnacio.Aguilera@endava.com>
2020-07-13 18:18:32 -0300

    day 4 as copy of day 3

---
+++ lessons/day_04/netcore/src/GraphQLSample/GraphQLTypes/RootQuery.cs
@@ -0,0 +1,80 @@
+﻿using GraphQL.Types;
+using GraphQLNetCore.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphQLNetCore.GraphQLTypes
+{
+   public class RootQuery : ObjectGraphType
+   {
+      private readonly ISkillRepository _skillRepository;
+      private readonly IPersonRepository _personRepository;
+
+      public RootQuery(ISkillRepository skillRepository, IPersonRepository personRepository)
+      {
+         _skillRepository = skillRepository;
+         _personRepository = personRepository;
+
+         Field<ListGraphType<SkillType>>("skills",
+            arguments: new QueryArguments
+            {
+                   new  QueryArgument<IntGraphType> {  Name = "id" }
+            },
+            resolve: context =>
+            {
+               var id = context.GetArgument<int?>("id");
+               return id.HasValue
+                  ? AsList(_skillRepository.Get(id))
+                  : _skillRepository.GetAll();
+            });
+
+         Field<SkillType>("randomSkill", resolve: context => _skillRepository.GetRandom());
+
+         Field<SkillType>("skill",
+             arguments: new QueryArguments
+             {
+                   new  QueryArgument<IntGraphType> {  Name = "id" }
+             },
+             resolve: context =>
+             {
+                var id = context.GetArgument<int?>("id");
+                return _skillRepository.Get(id);
+             });
+
+         Field<ListGraphType<PersonType>>("persons",
+            arguments: new QueryArguments
+            {
+               new  QueryArgument<IntGraphType> {  Name = "id" }
+            },
+            resolve: context =>
+            {
+               var id = context.GetArgument<int?>("id");
+               return id.HasValue
+                  ? AsList(_personRepository.Get(id))
+                  : _personRepository.GetAll();
+            });
+
+         Field<PersonType>("randomPerson", resolve: context => _personRepository.GetRandom());
+
+         Field<PersonType>("person",
+             arguments: new QueryArguments
+             {
+                   new  QueryArgument<IntGraphType> {  Name = "id" }
+             },
+             resolve: context =>
+             {
+                var id = context.GetArgument<int?>("id");
+                return _personRepository.Get(id);
+             });
+      }
+
+      private IEnumerable<T> AsList<T>(T item)
+         where T : class
+      {
+         return item == default
+            ? new List<T>()
+            : Enumerable.Repeat(item, 1)
+            ;
+      }
+   }
+}
---
+++ lessons/day_04/netcore/src/GraphQLSample/Models/Skill.cs
@@ -0,0 +1,30 @@
+﻿namespace GraphQLNetCore.Models
+{
+    public class Skill
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int? parentId { get; set; }
+        public virtual Skill parent { get; set; }
+    }
+
+    public class SkillData
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string parent { get; set; }
+
+        public static Skill ToEntity(SkillData data)
+        {
+            if (data == default)
+                return default;
+
+            return new Skill()
+            {
+                id = int.Parse(data.id),
+                name = data.name,
+                parentId = int.TryParse(data.parent, out int parsedValue) ? parsedValue : default(int?)
+            };
+        }
+    }
+}
