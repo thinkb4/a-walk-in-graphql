1969b3b14c45dc4e37d77776166f08b917295798
Ignacio Aguilera <jiaguilera@gmail.com>
2020-07-25 22:17:52 -0300

    imported day 04 changes

---
+++ lessons/day_04/netcore/src/GraphQLSample/Models/InputPerson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GraphQLNetCore.Models
+{
+   public class InputPerson
+   {
+      public int? Id { get; set; }
+      public int? Age { get; set; }
+      public EyeColor? EyeColor { get; set; }
+      public int? FavSkill { get; set; }
+
+      public static InputPerson FromId(int? id) => id.HasValue ? new InputPerson { Id = id } : default;
+
+      public Func<Person, bool> Predicate => 
+         (person) => (!Id.HasValue || Id.Value == person.Id)
+                  && (!Age.HasValue || Age.Value == person.Age)
+                  && (!EyeColor.HasValue || EyeColor.Value == person.EyeColor)
+                  && (!FavSkill.HasValue || FavSkill.Value == person.FavSkillId)
+                  ;
+   }
+}
---
+++ lessons/day_04/netcore/src/GraphQLSample/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GraphQLNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args) => BuildWebHost(args).Run();
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        }
+
+    }
+}
